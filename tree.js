782b9023e893d32b0f258f42509114213b621186
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-08-11 17:14:55 +1000

    Create buildTree method for Tree class

--- tree.js
+++ tree.js
@@ -7,53 +7,33 @@ export default class Tree {
     // root
   }
 
-  buildTree(arr) {
-    // let newArr = [...new Set(arr)].sort((a, b) => a-b)
-    let newArr = Array.from(arr)
-    console.log(newArr);
+  buildTree(arr, start, end) {
+    let newArr = [...new Set(arr)] // remove duplicate values from array
+    newArr.sort( (a, b) => a - b ) // sort array numerically
     
-    let nodeArr = []
-    // const rootIndex = Math.round((arr.length - 1) / 2) // get middle index
-    let rootIndex = 0
-    let rootNode = new Node(newArr[rootIndex])
-    nodeArr.push(rootNode)
-    
-    let currentNode = rootNode
-    for (let i = 0; i < newArr.length; i++) {
-      if (i === rootIndex) continue
-      
-      let isNull = false
-      const newNode = new Node(newArr[i])
-      console.log('\nindex', i);
-      do {
-        if (newNode.data < currentNode.data) {
-          if (currentNode.left !== null) {
-            currentNode = currentNode.left // traverse to left is there is an existing child node
-          } else {
-            currentNode.left = newNode
-            console.log('newNode', newNode);
-            console.log('currentNode', currentNode);
-            isNull = true
-          }
-        } else if (newNode.data >= currentNode.data) {
-          if (currentNode.right !== null) {
-            currentNode = currentNode.right // traverse to right is there is an existing child node
-          } else {
-            currentNode.right = newNode
-            console.log('newNode', newNode);
-            console.log('currentNode', currentNode);
-            isNull = true
-          }
-        }
-        nodeArr.push(newNode)
-      } while (!isNull)
+    if (start > end) {
+      return null
     }
+
+    const midIndex = parseInt((start + end) / 2) // get middle index
+    let rootNode = new Node(newArr[midIndex])
+
+    rootNode.left = this.buildTree(arr, start, midIndex - 1)
+    rootNode.right = this.buildTree(arr, midIndex + 1, end)
+
     return rootNode
   }
+
 }
 
 
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+let cleanedArr = [...new Set(arr)].sort( (a, b) => a - b );
+const n = cleanedArr.length
+console.log(cleanedArr);
+
 const tree = new Tree()
-const root = tree.buildTree(arr)
+const root = tree.buildTree(arr, 0, n - 1)
+
+console.log(root);
 prettyPrint(root)
